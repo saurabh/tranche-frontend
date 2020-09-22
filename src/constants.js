d43d07f2c93f9202728e8f3a80e79602c1cb2885
Saurabh W <w.saurabh@protonmail.com>
2020-09-23 01:12:52 +0400

    fixed constructor

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
+// const JLoansEthDeployerAddress = '0xE6d3E2A19c1a1B61E26982fdFa081a9Fbf68a010';
+// const JLoansTokenDeployerAddress = '0x281Fc9c46b06440e3D4aF8Ef50Dfb4576fD64E4f';
+// const JFeesCollectorAddress = '0x4d116D767222cc1424a17D5C41C769eAD95E1BF4';
 const JFactoryAddress = '0x78cf587217A3C4f0cFA30598AC34DDD4C20Bf69A';
 
 export { JFactoryAddress };
--- src/utils/JFactoryConstructor.js
+++ src/utils/JFactoryConstructor.js
@@ -1,5 +1,5 @@
 import abi from './JFactoryABI';
-import JFactoryAddress from 'constants';
+import { JFactoryAddress } from 'constants';
 
 export default function JFactorySetup(web3) {
   return new web3.eth.Contract(abi, JFactoryAddress);
