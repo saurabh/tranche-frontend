b0870e6a95db74bdd716078142f85cfe05923fc8
hg-tranche <70196848+hg-tranche@users.noreply.github.com>
2020-09-25 11:44:55 +0400

    removed unnessary file

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import { GlobalStyle } from 'components/common';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { GlobalStyle } from "components/common";
 
 // Routes
-import Home from 'components/pages/Home';
-import Earn from 'components/pages/Earn';
-import Borrow from 'components/pages/Borrow';
-import Trade from 'components/pages/Trade';
-import NotFound from 'components/pages/NotFound';
+import Earn from "components/pages/Earn";
+import Borrow from "components/pages/Borrow";
+import Trade from "components/pages/Trade";
+import NotFound from "components/pages/NotFound";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from '../redux/store';
+import { Provider } from "react-redux";
+import store from "../redux/store";
 import "../App.css";
-
 
 const App = () => {
   return (
@@ -22,9 +25,9 @@ const App = () => {
       <Router>
         <Switch>
           <Redirect exact from="/" to="/borrow" />
-          <Route exact path='/earn' component={Earn} />
-          <Route exact path='/borrow' component={Borrow} />
-          <Route exact path='/trade' component={Trade} />
+          <Route exact path="/earn" component={Earn} />
+          <Route exact path="/borrow" component={Borrow} />
+          <Route exact path="/trade" component={Trade} />
           <Route component={NotFound} />
         </Switch>
       </Router>
