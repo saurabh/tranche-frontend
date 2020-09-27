becab362279245cc155ed92a9729b10260abb693
hg-tranche <70196848+hg-tranche@users.noreply.github.com>
2020-09-27 11:05:48 +0400

    Integrate listing with new design

--- src/components/common/Table/Table.js
+++ src/components/common/Table/Table.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import TableHeader from "./TableHeader";
 import TableHead from "./TableHead";
 import TableCard from "./TableCard";
-import styled from 'styled-components';
+import styled from "styled-components";
+import LoanService from "services/loan.service";
 
 const TableWrapper = styled.div`
   width: 100%;
-  background: #FFFFFF;
-  border: 1px solid #EFEFEF;
+  background: #ffffff;
+  border: 1px solid #efefef;
   box-sizing: border-box;
   margin: 24px auto;
   border-radius: 12px;
 `;
-function Table({HandleNewLoan}){
-  const number = 9;
+
+function Table({ HandleNewLoan }) {
+  const [loanList, setLoanList] = useState([]);
+
+  useEffect(() => {
+    loanListing();
+  }, []);
+
+  const loanListing = async () => {
+    const loanListResult = await LoanService.loanList({
+      skip: 0,
+      limit: 10000,
+      filter: {
+        type: null,
+      },
+    });
+    if (loanListResult && loanListResult.result) {
+      const { list } = loanListResult.result;
+      setLoanList(list);
+    }
+  };
+
   return (
     <div className="container content-container">
       <TableWrapper>
-          <TableHeader HandleNewLoan={HandleNewLoan}/>
-          <div className="table-container">
-              <TableHead />
-              <div className="table-content">
-                {
-                  [...Array(number)].map((card, i) => <TableCard key={i}/>)
-                }
-              </div>
+        <TableHeader HandleNewLoan={HandleNewLoan} />
+        <div className="table-container">
+          <TableHead />
+          <div className="table-content">
+            {loanList.map((loan, i) => (
+              <TableCard key={i} loan={loan} />
+            ))}
           </div>
+        </div>
       </TableWrapper>
     </div>
   );
