f0a49e7972f3060e30c0d84e92bf8d9f34f88969
Saurabh W <w.saurabh@protonmail.com>
2020-10-01 01:17:45 +0400

    Added JLoanABIs

--- src/utils/contractConstructor.js
+++ src/utils/contractConstructor.js
@@ -1,8 +1,16 @@
-import { JFactoryABI, ERC20ABI } from 'ABIs';
+import { JFactoryABI, JLoanEthABI, JLoanTokenABI, ERC20ABI } from 'ABIs';
 import { JFactoryAddress, JPTAddress, USDCAddress } from 'config/ethereum';
 
 export function JFactorySetup(web3) {
   return new web3.eth.Contract(JFactoryABI, JFactoryAddress);
+}
+
+export function JLoanEthSetup(web3, address) {
+  return new web3.eth.Contract(JLoanEthABI, address);
+}
+
+export function JLoanTokenSetup(web3, address) {
+  return new web3.eth.Contract(JLoanTokenABI, address);
 }
 
 export function JPTSetup(web3) {
