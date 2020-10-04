b9da84358fdbe5d71c4ccbc945b0e58da8625f68
Saurabh W <w.saurabh@protonmail.com>
2020-10-04 10:29:18 +0400

    redeployed contracts

--- src/config/ethereum.js
+++ src/config/ethereum.js
@@ -1,8 +1,8 @@
 export const networkId = 42;
-export const JFactoryAddress = '0x5e6ede7f9a5E5ea0c3872991797170f1517609D2';
-export const JLoanEthDeployerAddress = '0x172BAfAaF0Df44dA3028E3049D7c688Ea5FBFb14';
-export const JLoanTokenDeployerAddress = '0xBfDFFfCe34537D370265aFdA6C2Da0464e0C3656';
-export const JFeesCollectorAddress = '0x9d90c04EC76814A4a836484E9452322327E6fCb5';
+export const JFactoryAddress = '0x8665fb6eEd5e3122a0281dBFc52507041eEd5AbF';
+export const JLoanEthDeployerAddress = '0x9EF23D74CDbd2c82b051D5bf31AB32D0C92a32F4';
+export const JLoanTokenDeployerAddress = '0x3e09BdB04d66f7109bcE7Fde095A1284916A2509';
+export const JFeesCollectorAddress = '0x4dFC643D36709F3C4e7F4E3ec37D155694eac555';
 export const DAIAddress = '0xe9f78fb3450Bc18866c5A94EAFbb2D305c4015B3';
 export const JPTAddress = '0xD8DBE56Bd586EF031bfdC96c467C065384c7C21c';
 export const USDCAddress = '0x19046f91357Ff5fa028634f092B1f8F1E82d4350';
