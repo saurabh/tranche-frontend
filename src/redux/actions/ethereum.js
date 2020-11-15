c08313f7307c5d4ceb1faf92c63bf17a44de1b41
Saurabh W <w.saurabh@protonmail.com>
2020-11-15 15:02:01 +0400

    added setTokenBalance

--- src/redux/actions/ethereum.js
+++ src/redux/actions/ethereum.js
@@ -1,14 +1,22 @@
 import Web3 from 'web3';
 import { web3 } from 'utils/getWeb3';
+import { pairData } from 'config/constants'
 import { DAISetup, JPTSetup, USDCSetup } from 'utils/contractConstructor'
 import {
   SET_ADDRESS,
   SET_NETWORK,
   SET_BALANCE,
+  SET_TOKEN_BALANCE,
   SET_TOKEN_BALANCES,
   SET_WALLET,
   SET_WEB3,
 } from './constants';
+
+const searchArr = (key) => pairData.find((i) => i.key === key);
+
+const timeout = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 export const setAddress = (address) => (dispatch) => {
   dispatch({
@@ -28,6 +36,18 @@ export const setBalance = (balance) => (dispatch) => {
   dispatch({
     type: SET_BALANCE,
     payload: balance
+  });
+};
+
+export const setTokenBalance = (tokenName, address) => async (dispatch) => {
+  await timeout(3000);
+  const { lendTokenSetup } = searchArr(tokenName);
+  const lendToken = lendTokenSetup(web3);
+  const tokenBalance = await lendToken.methods.balanceOf(address).call();
+
+  dispatch({
+    type: SET_TOKEN_BALANCE,
+    payload: { tokenName, tokenBalance }
   });
 };
 
