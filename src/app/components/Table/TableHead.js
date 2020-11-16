6b513e0ba99ae0f40896f8199f5ff18001b820c4
Mohamed Elgazzar <thisisgazzar@gmail.com>
2020-11-16 11:01:02 +0200

    borrower-lender-tags/structure-modifications/collateral-balance-design/some-styling-edits

---
+++ src/app/components/Table/TableHead.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { downChevron, upChevron } from 'assets';
+import {
+    TableHeadWrapper,
+    TableHeadTitle,
+    SortChevronWrapper,
+  } from './styles/TableComponents';
+
+
+const TableHead = ({handleSorting}) => {
+
+    const [order, setOrder] = useState("asc")
+
+    const sortLoans = (val) => {
+        handleSorting(val, order)
+        setOrder(order === "asc" ? "desc" : "asc")
+    }
+    return (
+        <TableHeadWrapper>
+            <TableHeadTitle className="address-wrapper" defaultCursor={true}>
+                <div className="address-title-content">
+                    <h2>Address</h2>
+                </div>
+            </TableHeadTitle>
+            <TableHeadTitle className="remaining-wrapper">
+                <div className="remaining-title-content" onClick={() => sortLoans("remainingLoan")}>
+                    <h2>Amount</h2>
+                    <SortChevronWrapper>
+                        <img src={upChevron} />
+                        <img src={downChevron} />
+                    </SortChevronWrapper>
+                </div>
+            </TableHeadTitle>
+            <TableHeadTitle className="ratio-wrapper">
+                <div className="ratio-title-content" onClick={() => sortLoans("collateralRatio")}>
+                    <h2>Ratio</h2>
+                    <SortChevronWrapper>
+                        <img src={upChevron} />
+                        <img src={downChevron} />
+                    </SortChevronWrapper>
+                </div>
+            </TableHeadTitle>
+            <TableHeadTitle className="interest-paid-wrapper">
+                <div className="interest-paid-title-content" onClick={() => sortLoans("interestPaid")}>
+                    <h2>APY</h2>
+                    <SortChevronWrapper>
+                        <img src={upChevron}/>
+                        <img src={downChevron}/>
+                    </SortChevronWrapper>
+                </div>
+            </TableHeadTitle>
+            <TableHeadTitle className="status-wrapper">
+                <div className="status-title-content" onClick={() => sortLoans("status")}>
+                    <h2>Status</h2>
+                    <SortChevronWrapper>
+                        <img src={upChevron}/>
+                        <img src={downChevron}/>
+                    </SortChevronWrapper>
+                </div>
+            </TableHeadTitle>
+            <TableHeadTitle className="head-btns-wrapper">
+
+            </TableHeadTitle>
+        </TableHeadWrapper>  
+    );
+}
+
+export default TableHead;
---
+++ src/app/pages/Borrow.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+  setAddress,
+  setNetwork,
+  setBalance,
+  setWalletAndWeb3
+} from 'redux/actions/ethereum';
+import { initOnboard } from 'services/blocknative';
+import { readyToTransact } from 'utils/helperFunctions';
+import { Layout } from 'app/components';
+import CreateLoan from 'app/components/Modals/CreateLoan';
+import { PagesData } from 'config/constants';
+import Table from '../components/Table/Table';
+import { changeFilter } from 'redux/actions/loans';
+
+const Borrow = ({
+  setAddress,
+  setNetwork,
+  setBalance,
+  setWalletAndWeb3,
+  ethereum: { address, network, balance, wallet, web3 },
+  changeFilter
+}) => {
+  const { pathname } = useLocation();
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    changeFilter(null);
+  }, [pathname, changeFilter]);
+
+  const onboard = initOnboard({
+    address: setAddress,
+    network: setNetwork,
+    balance: setBalance,
+    wallet: setWalletAndWeb3
+  });
+
+  const handleNewLoanClick = async () => {
+    const ready = await readyToTransact(wallet, onboard);
+    if (!ready) return;
+    setShowModal(true);
+  };
+
+  return (
+    <Layout>
+      <Table
+        HandleNewLoan={handleNewLoanClick}
+        openModal={showModal}
+        pageType={PagesData.borrow.pageType}
+      />
+      <CreateLoan openModal={showModal} closeModal={() => setShowModal(false)} />
+    </Layout>
+  );
+};
+
+Borrow.propTypes = {
+  ethereum: PropTypes.object.isRequired,
+  setAddress: PropTypes.func.isRequired,
+  setNetwork: PropTypes.func.isRequired,
+  setBalance: PropTypes.func.isRequired,
+  setWalletAndWeb3: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  ethereum: state.ethereum,
+});
+
+export default connect(mapStateToProps, {
+  setAddress,
+  setNetwork,
+  setBalance,
+  setWalletAndWeb3,
+  changeFilter
+})(Borrow);
