49096a0fe8ffab6799eaa8a431e43c9d8a9faa10
hg-tranche <70196848+hg-tranche@users.noreply.github.com>
2020-12-06 15:27:55 +0400

    remove build folder

--- build/static/js/51.56a58b13.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpjibrel-defi-dapp"]=this["webpackJsonpjibrel-defi-dapp"]||[]).push([[51,52],{1198:function(p,i){}}]);
-//# sourceMappingURL=51.56a58b13.chunk.js.map
--- build/static/js/8.72bd67a6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpjibrel-defi-dapp"]=this["webpackJsonpjibrel-defi-dapp"]||[]).push([[8],{1568:function(p,i){}}]);
-//# sourceMappingURL=8.72bd67a6.chunk.js.map
