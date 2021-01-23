7c7c943e73ae379927c8ca69bc3c1ec5a4a4e73d
Mohamed Elgazzar <thisisgazzar@gmail.com>
2021-01-23 13:26:44 +0400

    howToLinks/Banner/Styling

--- src/app/components/Header/HeaderTabs.js
+++ src/app/components/Header/HeaderTabs.js
@@ -167,7 +167,7 @@ const HeaderTabs = ({ path, changeOwnAllFilter, sellBuyToggle, ethereum: { addre
             </HeaderTabBtn>
           </RatesWrapper>
           <div>
-            <HeaderTabBtn link as='a' href='https://docs.tranche.finance' target='_blank' id='how-to-tab'>
+            <HeaderTabBtn link as='a' href={" https://docs.tranche.finance/tranche-finance/use/" + (path === "borrow" ? "borrowing" : path === "earn" ? "earning" : "")} target='_blank' id='how-to-tab'>
               How-to
             </HeaderTabBtn>
           </div>
