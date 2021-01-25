600a31fd8e5fd440f3fc01d543c45b3dad444a3c
Mohamed Elgazzar <thisisgazzar@gmail.com>
2021-01-25 13:04:47 +0400

    conflicts

--- src/app/pages/Trade.js
+++ src/app/pages/Trade.js
@@ -2,17 +2,13 @@ import React from 'react';
 import { Layout } from 'app/components';
 import SummaryCards from '../components/Summary/SummaryCards';
 import Table from '../components/Table/Table';
-import { Redirect } from 'react-router-dom';
 import { PagesData } from 'config/constants';
 
 export default function Trade() {
-  if(true){
-    return <Redirect to="/borrow" />
-  }
   return (
     <Layout>
       <SummaryCards />
-      <Table pageType={PagesData.trade.pageType} />
+      <Table pageType={PagesData.earn.pageType} />
     </Layout>
   );
 }
