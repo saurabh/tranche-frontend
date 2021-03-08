3e981cb1525568d3b9b5182f385cef4f1e8dd84d
hg-tranche <70196848+hg-tranche@users.noreply.github.com>
2021-03-08 11:53:46 +0400

    fixed discord link

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -7,7 +7,7 @@ export function Footer() {
         <div className="footerLinks">
           <a href="/privacy" target="_blank">Privacy</a> 
           <a href="/terms" target="_blank">Terms</a>
-          <a href="https://discord.gg/DTZrm4j4Yc" target="_blank" rel="noopener noreferrer">Support</a>
+          <a href="https://discord.gg/Nv44PTdF3K" target="_blank" rel="noopener noreferrer">Support</a>
           <a href="https://docs.tranche.finance" target="_blank" rel="noopener noreferrer">Documentation</a>
           <a href="https://github.com/tranche-jibrel/loan-contracts" target="_blank" rel="noopener noreferrer">Github</a>
         </div>
