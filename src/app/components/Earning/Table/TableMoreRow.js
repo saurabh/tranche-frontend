d281e1dca71414ac67a8ee8ea9df28e608309247
Saurabh <sw@tranche.finance>
2021-03-16 23:28:03 +0400

    added form WIP

--- src/app/components/Earning/Table/TableMoreRow.js
+++ src/app/components/Earning/Table/TableMoreRow.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Form, Field, reduxForm, getFormValues } from 'redux-form';
 import {
   TableMoreRowWrapper,
   TableMoreRowContent,
@@ -10,40 +12,54 @@ import {
   BtnArrow
 } from 'assets';
 
-const TableMoreRow = () => {
+let TableMoreRow = () => {
   return (
-    <TableMoreRowWrapper className='table-more-row'>
-      <TableMoreRowContent>
-        <TableMoreRowContentLeft>
-          <TableMoreLeftSection>
-            <h2>deposit</h2>
-            <h2>balance: 103,123 DAI</h2>
-            <form>
-              <div>
-                <input type="number" />
-                <button>max</button>
-              </div>
-              <button type="submit"><img src={BtnArrow} alt="arrow" />deposit</button>
-            </form>
-          </TableMoreLeftSection>
-          <TableMoreLeftSection withdraw>
-            <h2>withdraw</h2>
-            <h2>balance: 3,528 TACDAI</h2>
-            <form>
-              <div>
-                <input type="number" />
-                <button>max</button>
-              </div>
-              <button type="submit"><img src={BtnArrow} alt="arrow" />withdraw</button>
-            </form>
-          </TableMoreLeftSection>
-        </TableMoreRowContentLeft>
-        <TableMoreRowContentRight>
+    <Form>
+      <TableMoreRowWrapper className='table-more-row'>
+          <TableMoreRowContent>
+            <TableMoreRowContentLeft>
+              <TableMoreLeftSection>
+                <h2>deposit</h2>
+                <h2>balance: 103,123 DAI</h2>
+                <form>
+                  <div>
+                    <input type="number" />
+                    <button>max</button>
+                  </div>
+                  <button type="submit"><img src={BtnArrow} alt="arrow" />deposit</button>
+                </form>
+              </TableMoreLeftSection>
+              <TableMoreLeftSection withdraw>
+                <h2>withdraw</h2>
+                <h2>balance: 3,528 TACDAI</h2>
+                <form>
+                  <div>
+                    <input type="number" />
+                    <button>max</button>
+                  </div>
+                  <button type="submit"><img src={BtnArrow} alt="arrow" />withdraw</button>
+                </form>
+              </TableMoreLeftSection>
+            </TableMoreRowContentLeft>
+            <TableMoreRowContentRight>
 
-        </TableMoreRowContentRight>
-      </TableMoreRowContent>
-    </TableMoreRowWrapper>
+            </TableMoreRowContentRight>
+          </TableMoreRowContent>
+      </TableMoreRowWrapper>
+    </Form>
   );
 };
 
-export default TableMoreRow;
+
+TableMoreRow = reduxForm({
+  form: 'earn',
+  enableReinitialize: true
+})(TableMoreRow);
+
+const mapStateToProps = (state) => ({
+  ethereum: state.ethereum,
+  initialValues: {},
+  formValues: getFormValues('earn')(state)
+});
+
+export default TableMoreRow = connect(mapStateToProps, {})(TableMoreRow);
