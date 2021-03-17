9dc3306b8681afb1fa09ce54123a04ad91d044bd
Mohamed Elgazzar <thisisgazzar@gmail.com>
2021-03-17 14:58:31 +0200

    graph/translations-locale/edits/fixingBugs/styling

--- src/app/components/Earning/Table/TableMoreRow.js
+++ src/app/components/Earning/Table/TableMoreRow.js
@@ -25,60 +25,58 @@ const InputField = ({ input, type, className, meta: { touched, error } }) => (
 
 let TableMoreRow = ({ earnAllowanceCheck, earnApproveContract, buySellTrancheTokens }) => {
   return (
-    <Form>
-      <TableMoreRowWrapper className='table-more-row'>
-        <TableMoreRowContent>
-          <TableMoreRowContentLeft>
-            <TableMoreLeftSection>
-              <h2>deposit</h2>
-              <h2>balance: 103,123 DAI</h2>
-              {/* <Field component='input' type='checkbox' name='isApproved' /> */}
-              <form onSubmit={e=>e.preventDefault()}>
-                <div>
-                  <Field
-                    component={InputField}
-                    validate={[required, number]}
-                    // className='ModalFormInputNewLoan tradeFormInput'
-                    name='deposit'
-                    type='number'
-                    step='0.001'
-                  />
-                  <button>max</button>
-                </div>
-                <button type='submit'>
-                  <img src={BtnArrow} alt='arrow' />
-                  deposit
-                </button>
-              </form>
-            </TableMoreLeftSection>
-            <TableMoreLeftSection withdraw>
-              <h2>withdraw</h2>
-              <h2>balance: 3,528 TACDAI</h2>
-              <form onSubmit={e=>e.preventDefault()}>
-                <div>
-                  <Field
-                    component={InputField}
-                    validate={[required, number]}
-                    // className='ModalFormInputNewLoan tradeFormInput'
-                    name='withdraw'
-                    type='number'
-                    step='0.001'
-                  />
-                  <button>max</button>
-                </div>
-                <button type='submit'>
-                  <img src={BtnArrow} alt='arrow' />
-                  deposit
-                </button>
-              </form>
-            </TableMoreLeftSection>
-          </TableMoreRowContentLeft>
-          <TableMoreRowContentRight>
-            <Chart />
-          </TableMoreRowContentRight>
-        </TableMoreRowContent>
-      </TableMoreRowWrapper>
-    </Form>
+    <TableMoreRowWrapper className='table-more-row'>
+      <TableMoreRowContent>
+        <TableMoreRowContentLeft>
+          <TableMoreLeftSection>
+            <h2>deposit</h2>
+            <h2>balance: 103,123 DAI</h2>
+            {/* <Field component='input' type='checkbox' name='isApproved' /> */}
+            <Form>
+              <div>
+                <Field
+                  component={InputField}
+                  validate={[required, number]}
+                  // className='ModalFormInputNewLoan tradeFormInput'
+                  name='deposit'
+                  type='number'
+                  step='0.001'
+                />
+                <button>max</button>
+              </div>
+              <button type='submit'>
+                <img src={BtnArrow} alt='arrow' />
+                deposit
+              </button>
+            </Form>
+          </TableMoreLeftSection>
+          <TableMoreLeftSection withdraw>
+            <h2>withdraw</h2>
+            <h2>balance: 3,528 TACDAI</h2>
+            <Form>
+              <div>
+                <Field
+                  component={InputField}
+                  validate={[required, number]}
+                  // className='ModalFormInputNewLoan tradeFormInput'
+                  name='withdraw'
+                  type='number'
+                  step='0.001'
+                />
+                <button>max</button>
+              </div>
+              <button type='submit'>
+                <img src={BtnArrow} alt='arrow' />
+                deposit
+              </button>
+            </Form>
+          </TableMoreLeftSection>
+        </TableMoreRowContentLeft>
+        <TableMoreRowContentRight>
+          <Chart />
+        </TableMoreRowContentRight>
+      </TableMoreRowContent>
+    </TableMoreRowWrapper>
   );
 };
 
