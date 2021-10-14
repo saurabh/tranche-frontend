b6911350865183f16b6ff0d5585f905d1a0c6266
Saurabh <sw@tranche.finance>
2021-10-14 15:09:23 +0400

    tranche analytics link changes

--- src/app/components/Earning/Table/TableCard.js
+++ src/app/components/Earning/Table/TableCard.js
@@ -7,7 +7,7 @@ import { setTokenBalances } from 'redux/actions/ethereum';
 import { trancheCardToggle } from 'redux/actions/tableData';
 import { checkServer } from 'redux/actions/checkServer';
 import { roundNumber, safeDivide, safeMultiply, searchTokenDecimals } from 'utils';
-import { statuses, trancheIcons, ModeThemes, etherScanUrl, maticBlockExplorerUrl } from 'config';
+import { statuses, trancheIcons, ModeThemes, etherScanUrl, landingUrl, maticBlockExplorerUrl } from 'config';
 import { LinkArrow, ChevronTable } from 'assets';
 import TableMoreRow from './TableMoreRow';
 
@@ -146,7 +146,7 @@ const TableCard = ({
               </TableCardImg>
               <FirstColContent instrument>
                 <FirstColTitle color={ModeThemes[theme].tableText}>
-                <a href={`https://app.tranche.finance/analytics/${network}/${trancheId}`} target="_blank" rel="noopener noreferrer">{cryptoType && cryptoType}
+                <a href={`${landingUrl}analytics/${network}/${trancheId}/${type === 'TRANCHE_A' ? 0 : 1}`} target="_blank" rel="noopener noreferrer">{cryptoType && cryptoType}
                 <svg xmlns="http://www.w3.org/2000/svg" fill={ModeThemes[theme].tableText} viewBox="0 0 48 48" width="452px" height="452px"><path d="M 41.470703 4.9863281 A 1.50015 1.50015 0 0 0 41.308594 5 L 27.5 5 A 1.50015 1.50015 0 1 0 27.5 8 L 37.878906 8 L 22.439453 23.439453 A 1.50015 1.50015 0 1 0 24.560547 25.560547 L 40 10.121094 L 40 20.5 A 1.50015 1.50015 0 1 0 43 20.5 L 43 6.6894531 A 1.50015 1.50015 0 0 0 41.470703 4.9863281 z M 12.5 8 C 8.3754991 8 5 11.375499 5 15.5 L 5 35.5 C 5 39.624501 8.3754991 43 12.5 43 L 32.5 43 C 36.624501 43 40 39.624501 40 35.5 L 40 25.5 A 1.50015 1.50015 0 1 0 37 25.5 L 37 35.5 C 37 38.003499 35.003499 40 32.5 40 L 12.5 40 C 9.9965009 40 8 38.003499 8 35.5 L 8 15.5 C 8 12.996501 9.9965009 11 12.5 11 L 22.5 11 A 1.50015 1.50015 0 1 0 22.5 8 L 12.5 8 z"/></svg>
                 </a>
                 
@@ -297,7 +297,7 @@ const TableCard = ({
               <TableFirstColWrapper>
                 <FirstColContent instrument>
                   <FirstColTitle color={ModeThemes[theme].tableText} tranche>
-                    <a href={`https://app.tranche.finance/analytics/${network}/${trancheId}`} target="_blank" rel="noopener noreferrer">{cryptoType && cryptoType}
+                    <a href={`${landingUrl}analytics/${network}/${trancheId}`} target="_blank" rel="noopener noreferrer">{cryptoType && cryptoType}
                     <svg xmlns="http://www.w3.org/2000/svg" fill={ModeThemes[theme].tableText} viewBox="0 0 48 48" width="452px" height="452px"><path d="M 41.470703 4.9863281 A 1.50015 1.50015 0 0 0 41.308594 5 L 27.5 5 A 1.50015 1.50015 0 1 0 27.5 8 L 37.878906 8 L 22.439453 23.439453 A 1.50015 1.50015 0 1 0 24.560547 25.560547 L 40 10.121094 L 40 20.5 A 1.50015 1.50015 0 1 0 43 20.5 L 43 6.6894531 A 1.50015 1.50015 0 0 0 41.470703 4.9863281 z M 12.5 8 C 8.3754991 8 5 11.375499 5 15.5 L 5 35.5 C 5 39.624501 8.3754991 43 12.5 43 L 32.5 43 C 36.624501 43 40 39.624501 40 35.5 L 40 25.5 A 1.50015 1.50015 0 1 0 37 25.5 L 37 35.5 C 37 38.003499 35.003499 40 32.5 40 L 12.5 40 C 9.9965009 40 8 38.003499 8 35.5 L 8 15.5 C 8 12.996501 9.9965009 11 12.5 11 L 22.5 11 A 1.50015 1.50015 0 1 0 22.5 8 L 12.5 8 z"/></svg>
                     </a>
                     <AdustBtnWrapper className='adjust-btn-wrapper' chevron status={trancheCard.status && id === trancheCard.id}>
