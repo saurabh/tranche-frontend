b5900f8f4c9e0ac3a27faf97c8c4cdd2aa437ff3
Mohamed Elgazzar <thisisgazzar@gmail.com>
2021-11-08 17:17:31 +0200

    FixingHeaderTabsBugs/MergingIssues

--- src/app/components/Earning/Header/HeaderTabs.js
+++ src/app/components/Earning/Header/HeaderTabs.js
@@ -137,14 +137,18 @@ const HeaderTabs = ({ data, trancheMarketsToggle, setTxModalOpen, setTxModalStat
           market='compound'
           current={trancheMarket === 'compound'}
           onClick={() => trancheMarketsToggle('compound')}
+          span={ModeThemes[theme].TrancheBtnSpan}
           background={ModeThemes[theme].TrancheBtnBackground}
           backgroundActive={ModeThemes[theme].TrancheBtnBackgroundCurrent}
           border={ModeThemes[theme].TrancheBtnBorder}
           color={ModeThemes[theme].TrancheBtnColor}
           theme={theme}
           btnShadow={ModeThemes[theme].btnShadow}
+          mobile
         >
-          <img src={theme === 'light' ? CompoundBtnBlack : CompoundBtn} alt='' />
+          <img src={theme === 'light' ? CompoundBtnBlack : CompoundBtn} alt='' /> 
+          <span></span> 
+          <img src={ETHLOGO} alt='' />
         </MarketTab>
         <MarketTab
           market='aavePolygon'
@@ -175,12 +179,14 @@ const HeaderTabs = ({ data, trancheMarketsToggle, setTxModalOpen, setTxModalStat
           color={ModeThemes[theme].TrancheBtnColor}
           theme={theme}
           btnShadow={ModeThemes[theme].btnShadow}
+          mobile
         >
-          <h2>
-            <img src={AaveBtn} alt='' /> <span>Market</span>
-          </h2>{' '}
-          <span></span> <img src={theme === 'light' ? PolygonLogoBlack : PolygonLogo} alt='' />
+          <img src={theme === 'light' ? YEARNLOGOLIGHT : YEARNLOGO} alt='' /> 
+          <span></span> 
+          <img src={theme === 'light' ? FANTOMLOGOLIGHT : FANTOMLOGO} alt='' />
         </MarketTab>
+
+
       </Carousel>
       }
 
