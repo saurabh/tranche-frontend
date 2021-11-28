9d5c9ae6dc9b94c9ee91555d9e3855e890087841
Saurabh <sw@tranche.finance>
2021-11-28 12:41:21 +0400

    updated script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 git branch
 git checkout dev-build-3
 git pull origin dev-build-3
-git pull origin hotfix
+git pull origin ftmTranche
 yarn build
 git add .
 git commit -m 'new build'
 git push origin dev-build-3
-git checkout hotfix
+git checkout ftmTranche
