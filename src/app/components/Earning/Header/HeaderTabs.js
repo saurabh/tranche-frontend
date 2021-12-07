007c7d78ab48902608967f23579526bff090d815
Mohamed Elgazzar <thisisgazzar@gmail.com>
2021-12-07 08:42:58 +0200

    APYTooltip/MobileAnalyticsLinkBug/FantomBridging

--- src/app/components/Earning/Header/HeaderTabs.js
+++ src/app/components/Earning/Header/HeaderTabs.js
@@ -147,6 +147,16 @@ const HeaderTabs = ({ ethereum: { network }, data, trancheMarketsToggle, setTxMo
           </a>
         </BridgeTokensWrapper>
       )}
+      {tranchesToggle === 'fantom' && (
+        <BridgeTokensWrapper>
+          <p>
+            To use Fantom markets, you will need to use the Fantom bridge to move your tokens from the Ethereum mainnet to the Fantom. After moving your assets to Fantom, you can buy different instruments on Tranche, trade on the Quickswap DEX, and explore other applications on the Fantom network.
+          </p>
+          <a href='https://fantom.foundation/' target='_blank' rel='noopener noreferrer'>
+            <button>Bridge Tokens</button>
+          </a>
+        </BridgeTokensWrapper>
+      )}
       <TrancheModal closeModal={() => closeModal()} />
     </MarketsTabsWrapper>
   );
